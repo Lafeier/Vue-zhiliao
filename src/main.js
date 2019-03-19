31e310cfbaf5f5e1779174c69bb7e768f0472b1d
LFE <63526880@qq.com>
2019-03-19 19:38:12 +0800

    第三次提交-实现首页购物车小图标和tabbar路由相关功能

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 // 项目入口文件
 import Vue from 'vue';
+// 1.1 导入路由的包
+import VueRouter from 'vue-router';
+// 1.2 安装路由
+Vue.use(VueRouter);
 
 
 // 导入MUI的样式
 import './lib/mui/css/mui.min.css';
+// 导入MUI扩展图标的样式
+import './lib/mui/css/icons-extra.css';
 
 // 按需导入 Mint-UI 的组件
 import { Header } from 'mint-ui';
 Vue.component(Header.name, Header);
+
+
+// 1.3 导入自己的router.js路由模块
+import router from './router.js';
 
 
 // 导入 App根组件
@@ -15,7 +25,8 @@ import app from './App.vue';
 
 var vm = new Vue({
     el: '#app',
-    render: c => c(app)
+    render: c => c(app),
+    router  // 1.4 挂载路由到 vm 实例上
 });
 
 
--- src/router.js
+++ src/router.js
@@ -5,7 +5,8 @@ import VueRouter from 'vue-router';
 // 3.创建路由对象
 var router = new VueRouter({
     routes: [
-    ]
+    ],
+    linkActiveClass: 'mui-active'   // 覆盖默认的路由高亮类，默认的类名叫router-link-active
 });
 
 
